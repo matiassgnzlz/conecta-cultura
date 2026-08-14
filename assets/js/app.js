f09972b3c94e5c3fadf50444dcd92e8843843047
Matias Gonzalez <matia.gonzalezma@duocuc.cl>
2026-08-14 12:42:31 -0400

    feat: construir estructura semántica de inicio y nosotros, agregar noticias y primer detalle

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,4 @@ const botonBienvenida = document.querySelector("#boton-bienvenida");
 botonBienvenida.addEventListener("click", () => {
 alert("Durante el semestre construiremos Conecta Cultura paso a paso.");
 });
+
